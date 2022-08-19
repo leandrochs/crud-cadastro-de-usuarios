e47765f6d5d1b98c6f3ffd5350ece6b0a2c5901b
leandrochs <leandrochagask@gmail.com>
2022-08-19 15:19:18 -0300

    feat: adiciona arquivo para user model

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,17 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      fullName: DataTypes.STRING,
+      email: DataTypes.STRING,
+    },
+    {
+      underscored: true,
+      tableName: 'Users',
+    },
+  );
+
+  return User;
+};
+
+module.exports = User;
