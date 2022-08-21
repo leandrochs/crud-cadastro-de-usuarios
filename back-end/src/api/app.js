895a83543e2d66d441ac9168b795e7ee4aaa2136
leandrochs <leandrochagask@gmail.com>
2022-08-21 11:35:31 -0300

    feat: adiciona rotas para login e client

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const middlewareErro = require('../middleware/erro');
+
+const Route = require('../router');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/login', Route.login);
+app.use('/client', Route.client);
+app.use(middlewareErro);
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 module.exports = app;
---
+++ back-end/src/router/index.js
@@ -0,0 +1,2 @@
+module.exports.login = require('./loginRoute');
+module.exports.client = require('./clientRoute');
