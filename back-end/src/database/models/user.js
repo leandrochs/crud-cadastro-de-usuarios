70fb22c6da13afb0107183adf591e7ffd6c89b7f
leandrochs <leandrochagask@gmail.com>
2022-08-21 11:37:38 -0300

    feat: adiciona model sequelize para User

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -2,8 +2,8 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      fullName: DataTypes.STRING,
       email: DataTypes.STRING,
+      password: DataTypes.STRING,
     },
     {
       underscored: true,
