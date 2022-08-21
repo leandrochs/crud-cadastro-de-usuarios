130a662c67b86a7f412d8dc5c56b33203f714914
leandrochs <leandrochagask@gmail.com>
2022-08-21 18:02:39 -0300

    fix: lint

--- back-end/src/middleware/validate.js
+++ back-end/src/middleware/validate.js
@@ -1,15 +1,17 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required().messages({
-    'any.required': 'O campo email está vazio.',
-    'string.email': 'Email inválido',
+  email: Joi.string().email().required()
+    .messages({
+      'any.required': 'O campo email está vazio.',
+      'string.email': 'Email inválido',
   }),
-  password: Joi.string().min(6).max(10).required().messages({
-    'any.required': 'O campo senha está vazio.',
-    'string.min': 'A senha requer no mínimo {{#limit }} caracteres.',
-    'string.max': 'A senha aceita no máximo {{#limit }} caracteres',
-  }),
+  password: Joi.string().min(6).max(10).required()
+    .messages({
+      'any.required': 'O campo senha está vazio.',
+      'string.min': 'A senha requer no mínimo {{#limit }} caracteres.',
+      'string.max': 'A senha aceita no máximo {{#limit }} caracteres',
+    }),
 });
 
 const validateLogin = async (req, res, next) => {
