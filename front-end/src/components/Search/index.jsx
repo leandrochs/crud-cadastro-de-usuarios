1d02b63c033ae7f90436bfc9ec7f3202535395b0
leandrochs <leandrochagask@gmail.com>
2022-08-22 13:25:20 -0300

    feat: adiciona elementos da barra de busca

--- front-end/src/components/Search/index.jsx
+++ front-end/src/components/Search/index.jsx
@@ -1,7 +1,42 @@
+import mGlass from '../../assets/magnifying-glass.svg';
+import refresh from '../../assets/refresh.png';
+import './style.css';
+
 function Search() {
+
+    async function onClick(e) {
+      e.preventDefault();
+      const res = await loginApi(email, password);
+  
+      if (res.token) {
+        history.push('./user');
+        localStorage.setItem('token', JSON.stringify(res));
+      }
+  
+      if (res.error) {
+        setErrorMessage(res.error);
+      }
+    
+  }
+
   return (
-    <div>
-      <button className="bt-new" type="button">novo</button>
+    <div className="search-container">
+      <button className="btn-new" type="button">
+        <span className="plus">+</span>
+        Novo
+      </button>
+      <div className="img-container">
+        <img className="search-img" src={ mGlass } alt="Lupa de busca" />
+      </div>
+      <input
+        className="search-input"
+        type="text"
+        onChange={ ({ target }) => setEmail(target.value) }
+        placeholder="Pesquisar"
+      />
+      <button className="btn-refresh" type="button" onClick={onClick} >
+        <img className="refresh-img" src={ refresh } alt="Buscar" />
+      </button>
     </div>
   );
 }
