d3dccac105b4a06f78b256d92be07f24e5bfca9e
leandrochs <leandrochagask@gmail.com>
2022-08-22 11:23:21 -0300

    feat: adiciona arquivo de pagina de user

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
-import { Provider } from './context/Provider';
 import Login from './pages/Login';
+import User from './pages/User';
+import { Provider } from './context/Provider';
 
 function App() {
   return (
     <Provider>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/user" component={ User } />
       </Switch>
     </Provider>
   );
